20b14fb396ea129ed0095258b0865306d6105e93
minhduypham <minhduypham@kms-technology.com>
2022-10-24 17:14:22 +0700

    thymeleaf-expression

--- src/main/java/com/example/springbootComAndAuto/WebController.java
+++ src/main/java/com/example/springbootComAndAuto/WebController.java
@@ -6,8 +6,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class WebController {
+
+
+    @GetMapping("/profile")
+    public String profile(Model model)
+    {
+        List<Info> profile = new ArrayList<>();
+        profile.add(new Info("fullname","Pham Duy Minh"));
+        profile.add(new Info("email","hadtnt71@gmail.com"));
+
+        model.addAttribute("profile",profile);
+        return "profile";
+    }
     @GetMapping("/")
     public String index()
     {
