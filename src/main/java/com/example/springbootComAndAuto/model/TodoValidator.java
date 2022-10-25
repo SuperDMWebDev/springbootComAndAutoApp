6256612c8ea933302631806891500cca19419c0c
minhduypham <minhduypham@kms-technology.com>
2022-10-25 16:09:01 +0700

    test

--- src/main/java/com/example/springbootComAndAuto/model/TodoValidator.java
+++ src/main/java/com/example/springbootComAndAuto/model/TodoValidator.java
@@ -1,10 +1,15 @@
 package com.example.springbootComAndAuto.model;
 
-import java.util.*;
+import antlr.StringUtils;
+
+import java.util.Optional;
 
 // use for check if object is valid
 public class TodoValidator {
 
     //todo( title, detail) must differ to null;
-    return Optional.filter()
+    public boolean isValid(Todo todo)
+    {
+        return todo.getTitle()!="" && todo.getTitle()!=null;
+    }
 }
